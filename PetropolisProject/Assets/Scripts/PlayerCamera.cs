a8546002073a14aa52b16b4bbc13da7c1a6d323a
khs1871074 <khs1871074@gmail.com>
2023-03-20 19:23:13 +0900

    PlayerCamera 스크립트 수정
    기존 우클릭 후 움직임에서 상시 움직임으로 변경

--- PetropolisProject/Assets/Scripts/PlayerCamera.cs
+++ PetropolisProject/Assets/Scripts/PlayerCamera.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerCamera : MonoBehaviour
 {
-    private Vector3 offsets;                // 카메라의 좌표
+    private Vector3 startoffsets, offsets;                // 카메라의 초기 좌표, 현재 좌표
     Quaternion camTurnAngleY, camTurnAngleX; // 카메라의 회전 쿼터니언
 
     public float rotateSpeed = 5.0f;        // 카메라 회전 속도
@@ -20,17 +20,21 @@ public class PlayerCamera : MonoBehaviour
 
     void Start()
     {
-        offsets = new Vector3(offsetX, offsetY, offsetZ);
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.lockState = CursorLockMode.Confined;
+        startoffsets = new Vector3(offsetX, offsetY, offsetZ);
+        offsets = startoffsets;
     }
 
     void LateUpdate()
     {
-        if (Input.GetMouseButton(0)) // 마우스 좌클릭 시
-        {
-            camTurnAngleY = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * rotateSpeed, Vector3.up); // 마우스 X축 -> 좌우 회전(오브젝트 Y축)
-            camTurnAngleX = Quaternion.AngleAxis(Input.GetAxis("Mouse Y") * rotateSpeed, Vector3.left); // 마우스 Y축 -> 상하 회전(오브젝트 X축)
-            offsets = camTurnAngleY * camTurnAngleX * offsets;
-        }
+        //if (Input.GetMouseButton(0)) // 마우스 좌클릭 시
+        //{
+        camTurnAngleY = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * rotateSpeed, Vector3.up); // 마우스 X축 -> 좌우 회전(오브젝트 Y축)
+        camTurnAngleX = Quaternion.AngleAxis(Input.GetAxis("Mouse Y") * rotateSpeed, Vector3.left); // 마우스 Y축 -> 상하 회전(오브젝트 X축)
+        offsets = camTurnAngleY * camTurnAngleX * offsets;
+        //}
+
         // 타겟의 x, y, z 좌표에 카메라의 좌표를 더하여 카메라의 위치를 결정
         TargetPos = Target.transform.position + offsets;
 
