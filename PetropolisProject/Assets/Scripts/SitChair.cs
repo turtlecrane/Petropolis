587995b534cd490e21546b67d34e3bcdff6ae431
kr-user <jun980417@naver.com>
2023-03-29 20:04:03 +0900

    sitting interactive

---
+++ PetropolisProject/Assets/Scripts/SitChair.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+
+public class SitChair : MonoBehaviour
+{
+    private GameObject target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            target = GetClickedObject();
+
+            if (target.Equals(gameObject))
+            {
+                Debug.Log("의자인거 확인");
+
+            }
+        }
+    }
+    private GameObject GetClickedObject()
+    {
+        RaycastHit hit;
+        GameObject target = null;
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (true == (Physics.Raycast(ray.origin, ray.direction * 10, out hit)))
+        {
+            target = hit.collider.gameObject;
+        }
+        return target;
+    }
+}
