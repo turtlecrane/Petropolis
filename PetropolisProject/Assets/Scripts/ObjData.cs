eaf426019f1f258cc39dd5a4c46eb1402522e3d8
kr-user <jun980417@naver.com>
2023-04-19 14:09:24 +0900

    gitattributes 생성

--- PetropolisProject/Assets/Scripts/ObjData.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class ObjData : MonoBehaviour
-{
-    public int id;
-    public bool isNpc;//npc인지 아닌지 구분
-}
