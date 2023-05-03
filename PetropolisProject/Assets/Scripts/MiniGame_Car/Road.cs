5ee050899abe4d2137691346f221ed681e652fbe
kr-user <jun980417@naver.com>
2023-05-03 14:52:42 +0900

    미니게임(길건너친구들) 추가

--- PetropolisProject/Assets/Scripts/MiniGame_Car/Road.cs
+++ PetropolisProject/Assets/Scripts/MiniGame_Car/Road.cs
@@ -7,9 +7,9 @@ public class Road : MonoBehaviour
     public Car CloneTarget = null;
     public Transform GenerationPos = null;
 
-    public int GenerationPersent = 50;
+    public int GenerationPersent = 50;  // 차량 나오는 확률
 
-    public float CloneDelaySec = 1f;
+    public float CloneDelaySec = 1f;    // 딜레이 시간
 
     protected float NextSecToClone = 0f;
     
