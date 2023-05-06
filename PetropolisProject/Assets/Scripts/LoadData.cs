a8676d6f9cbc2b24340602df16c242a638044072
khs1871074 <khs1871074@gmail.com>
2023-05-06 14:43:27 +0900

    Camera 수정, 씬 이동간의 데이터 Save, Load 추가
    현재는 Cat을 상시로 넣어두고 있으나 추후 별도의 Instantiate로 Cat, Dog를 생성하는 방안을 생각 중

---
+++ PetropolisProject/Assets/Scripts/LoadData.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadData : MonoBehaviour
+{
+    private SaveData saveData;
+    private GameObject[] npc;
+    private GameObject player;
+    private Transform playerPos;
+    // Start is called before the first frame update
+
+    void Awake()
+    {
+        saveData = GameObject.Find("SaveData").GetComponent<SaveData>(); // SaveData 불러오기
+        SetPlayer();
+        playerPos = saveData.GetPlayerPos(); // PlayerPos 불러오기
+        player.transform.position = playerPos.position; // player에 이전 transform 적용
+        player.transform.rotation = playerPos.rotation;
+    }
+    void Start()
+    {
+        if (saveData.GetNpcLength() != 0) // 저장된 Npc 데이터가 있다면
+        {
+            npc = GameObject.FindGameObjectsWithTag("NPC");
+            for (int i = 0; i < saveData.GetNpcLength(); i++) 
+                // 기존에 저장된 Npc의 Id와 새로 생긴 Npc의 Id가 다르다면 저장된 Npc의 Id로 변환
+            {
+                if (npc[i].GetComponent<ObjData>().name == saveData.GetNpcName(i) &&
+                    npc[i].GetComponent<ObjData>().id != saveData.GetNpcId(i))
+                {
+                    npc[i].GetComponent<ObjData>().id = saveData.GetNpcId(i);
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    private void SetPlayer() // SaveData의 SetPlayer와 같음
+    {
+        if (GameObject.FindGameObjectWithTag("Cat") != null)
+        {
+            player = GameObject.FindGameObjectWithTag("Cat");
+        }
+        else
+        {
+            player = GameObject.FindGameObjectWithTag("Dog");
+        }
+    }
+}
--- PetropolisProject/Assets/Scripts/ObjData.cs
+++ PetropolisProject/Assets/Scripts/ObjData.cs
@@ -9,4 +9,29 @@ public class ObjData : MonoBehaviour
     public int id;
     public bool isNpc;//npc인지 아닌지 구분
     public bool isDoctor;
+    private int beforeId; // 변경되기 전의 Id
+    private SaveData saveData;
+
+    void Start()
+    {
+        beforeId = id;
+        saveData = GameObject.Find("SaveData").gameObject.GetComponent<SaveData>();
+    }
+
+    void Update()
+    {
+        if (isNpc) // Npc의 경우 Id 변경 여부를 체크
+        {
+            CheckId();
+        }
+    }
+
+    void CheckId()
+    {
+        if (beforeId != id) // 만약 Id가 변경되었다면
+        {
+            saveData.SearchAndSaveChangedId(gameObject); // SaveData에 저장
+        }
+        beforeId = id;
+    }
 }
